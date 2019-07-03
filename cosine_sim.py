033509f4165333ef64c85f4f34064f6291d37417
Rapino Manuel <mrapino@esecurity.prv>
2019-07-03 11:56:22 +0200

    Corrected references to cosine similarity

--- cosine_sim.py
+++ cosine_sim.py
@@ -19,14 +19,14 @@ class cosineSim:
             for num2,shingles2 in docs:
               if num1<num2:   
                intersection = len(shingles1.intersection(shingles2))
-               j_sim=intersection / float(len(shingles1) + len(shingles2) - intersection) 
-               if j_sim >= t:
+               c_sim=intersection / float(len(shingles1) + len(shingles2) - intersection) 
+               if c_sim >= t:
 
-                           #sim_docs.add(tuple([num1,num2,j_sim]))
+                           #sim_docs.add(tuple([num1,num2,c_sim]))
                            sim_docs.add(tuple([num1,num2]))
                                             
             
-        print "Jaccard similarity duplicate pairs: "+str(len(sim_docs))       
+        print "Cosine similarity duplicate pairs: "+str(len(sim_docs))       
         
         
         for a,b in sim_docs:            
@@ -44,19 +44,19 @@ if __name__ == "__main__":
     sh = Shingle("kijiji_Ads.tsv", 10)
     start = time.time()
 
-    J_sim = jaccardSim()
+    C_sim = cosineSim()
 
     print "Pairs which have at least 80% Jaccard similarity"
-    jac_list = J_sim.Jacc_sim(sorted(sh.shingle_list), 0.8)
+    cos_list = C_sim.Jacc_sim(sorted(sh.shingle_list), 0.8)
     
     
     out_file=open("jac_test.txt","w")
-    print len(jac_list)
-    for pair in J_sim.sim_docs:
+    print len(cos_list)
+    for pair in C_sim.sim_docs:
         out_file.write(str(pair)+"\n")
        
     end = (time.time() - start)
    
-    print len(jac_list)
+    print len(cos_list)
     print end
 
